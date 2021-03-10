766d7a26e7281d1186d49e96840bc3abcb46c89d
Sarah Willson <sarahnwillson@sarahs-air.lan>
2021-03-10 15:46:25 -0800

    custom routes and friendly_id

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio, only: %i[ show edit update destroy ]
 
     # GET /portfolios
     def index
@@ -10,21 +11,46 @@ class PortfoliosController < ApplicationController
         @portfolio_item = Portfolio.new
     end
 
+    # GET /portfolios/1/edit
+    def edit
+    end
+
+    # POST /portfolios
     def create
         @portfolio_item = Portfolio.new(portfolio_params)
 
         respond_to do |format|
-        if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
-        else
-            format.html {render :new}
+            if @portfolio_item.save
+                format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+            else
+                format.html {render :new}
+            end
         end
+    end
+
+    # PATCH/PUT /portfolios/1
+    def update
+        respond_to do |format|
+            if @portfolio_item.update(portfolio_params)
+                format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+            else
+                format.html {render :edit}
+            end
+        end
+    end
+
+    # DELETE /portfolios/1
+    def destroy
+        @portfolio_item.destroy
+        respond_to do |format|
+            format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+            format.json { head :no_content }
         end
     end
 
     private
         # Use callbacks to share common setup or constraints between actions.
-        def set_blog
+        def set_portfolio
             @portfolio_item = Portfolio.find(params[:id])
         end
 
